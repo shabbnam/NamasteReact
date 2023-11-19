598a0bda5295b5600e4f4a8332d806b7916d46dc
shabbnam <shabrnd@gmail.com>
2023-11-19 13:38:38 +0530

    wip

--- LayingFoundation/app.js
+++ LayingFoundation/app.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
+//React.createElement -> React Element (object) -> HTML element(render)
+const heading1 = React.createElement(
   "h1",
   { id: "heading" },
   "Hello ! from react code"
 );
+
+//React element
+// JSX -> React.createElement -> React Element (object) -> HTML element(render)
+const heading2 = <h1>Namaste React element!!!!!</h1>;
+
+// React component
+// React component - function that returns a piece of JSX code
+// class based components
+// function bsased components
+const HeadingComponent = (props) => (
+  <h1>
+    Namaste React component-- {props.id}
+    {heading2}
+  </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent id="456" />);
